5f890d0601a2174408afcae194387a9a13291e70
danielecolaci <danielecolaci@outlook.com>
2024-11-20 01:21:58 +0100

    Set image files, add header and root color variables

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from '../header/header.component';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
+  imports: [
+    HeaderComponent
+  ]
 })
 export class AppComponent {
   title = 'ehi';
